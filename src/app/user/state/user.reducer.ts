fb3a3b3bb25791b38d4af8d73da7482ca03c05d7
Magluf <sergio.hassm.magluf@accenture.com>
2019-06-18 14:21:21 -0300

    Finished the 'Strongly Typing the State' module.

---
+++ src/app/user/state/user.reducer.ts
@@ -0,0 +1,43 @@
+import { User } from "../user";
+import * as fromRoot from '../../state/app.state'
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+
+export interface State extends fromRoot.State {
+    users: UserState
+}
+
+export interface UserState {
+    maskUserName: boolean,
+    currentUser: User
+}
+
+const initialState: UserState = {
+    maskUserName: true,
+    currentUser: null
+}
+
+const getUserFeatureState = createFeatureSelector<UserState>('users');
+
+export const getMaskUserName = createSelector(
+    getUserFeatureState,
+    state => state.maskUserName
+)
+
+export const getCurrentUser = createSelector(
+    getUserFeatureState,
+    state => state.currentUser
+)
+
+const reducer = (state = initialState, action): UserState => {
+    switch (action.type) {
+        case 'MASK_USER_NAME':
+            return {
+                ...state,
+                maskUserName: action.payload
+            }
+        default:
+            return state;
+    }
+}
+
+export default reducer;
