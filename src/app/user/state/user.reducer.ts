702e2832c00e5158e5b34f612534b320d724d867
Magluf <sergio.hassm.magluf@accenture.com>
2019-06-18 14:32:26 -0300

    Fixed app.state, but still don't know why I need to have the UserState set there and not have the ProductState.

--- src/app/user/state/user.reducer.ts
+++ src/app/user/state/user.reducer.ts
@@ -1,43 +1,43 @@
 import { User } from "../user";
-import * as fromRoot from '../../state/app.state'
+import * as fromRoot from "../../state/app.state";
 import { createFeatureSelector, createSelector } from "@ngrx/store";
 
 export interface State extends fromRoot.State {
-    users: UserState
+  users: UserState;
 }
 
 export interface UserState {
-    maskUserName: boolean,
-    currentUser: User
+  maskUserName: boolean;
+  currentUser: User;
 }
 
 const initialState: UserState = {
-    maskUserName: true,
-    currentUser: null
-}
+  maskUserName: true,
+  currentUser: null
+};
 
-const getUserFeatureState = createFeatureSelector<UserState>('users');
+const getUserFeatureState = createFeatureSelector<UserState>("users");
 
 export const getMaskUserName = createSelector(
-    getUserFeatureState,
-    state => state.maskUserName
-)
+  getUserFeatureState,
+  state => state.maskUserName
+);
 
 export const getCurrentUser = createSelector(
-    getUserFeatureState,
-    state => state.currentUser
-)
+  getUserFeatureState,
+  state => state.currentUser
+);
 
 const reducer = (state = initialState, action): UserState => {
-    switch (action.type) {
-        case 'MASK_USER_NAME':
-            return {
-                ...state,
-                maskUserName: action.payload
-            }
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case "MASK_USER_NAME":
+      return {
+        ...state,
+        maskUserName: action.payload
+      };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
